3082a0f4fa0f0c55389e8b8abccdd33875d1a3e7
Aleksandr Shirkunov <alex.shirkunoff@gmail.com>
2025-05-04 23:47:16 +0500

    minor tweaks

---
+++ scripts/.config/scripts/open_in_ranger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# removes file:// prefix if present
+DIR="${1#file://}"
+
+# if it's a file, gets its directory
+if [ -f "$DIR" ]; then
+    DIR=$(dirname "$DIR")
+fi
+
+# launches ranger with foot opened
+foot --app-id ranger-float -T "ranger-popup" -e ranger "$DIR"
