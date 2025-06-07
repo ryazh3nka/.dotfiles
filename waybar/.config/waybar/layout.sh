1c8cf9b3926436482f991edc9e8cad4d640ceb2b
Aleksandr Shirkunov <alex.shirkunoff@gmail.com>
2025-06-07 15:42:29 +0500

    changed indentation x2

--- waybar/.config/waybar/layout.sh
+++ waybar/.config/waybar/layout.sh
@@ -3,13 +3,13 @@
 layout=$(hyprctl getoption general:layout | grep 'str:' | awk '{print $2}')
 
 case "$layout" in
-    master)
-        echo "[]="
-        ;;
-    dwindle)
-        echo "*="
-        ;;
-    *)
-        echo "??"
-        ;;
+	master)
+		echo "[]="
+		;;
+	dwindle)
+		echo "*="
+		;;
+	*)
+		echo "??"
+		;;
 esac
