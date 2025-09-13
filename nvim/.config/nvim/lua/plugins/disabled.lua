821a8eea0cf2c4e0e53aa7bca62212258c998fd9
Aleksandr Shirkunov <alex.shirkunoff@gmail.com>
2025-09-13 12:49:14 +0200

    [nvim] got rid of lua integration altogether

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,3 +1,6 @@
 return {
         { "akinsho/bufferline.nvim", enabled = false },
+        { "stevearc/conform.nvim", enabled = false },
+        { "folke/neoconf.nvim", enabled = false },
+        { "folke/neodev.nvim", enabled = false },
 }
