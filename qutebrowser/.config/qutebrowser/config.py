0e2fb36b4ba665e926703d863339a69f8748ec27
Aleksandr Shirkunov <alex.shirkunoff@gmail.com>
2025-10-04 20:21:50 +0200

    [qutebrowser] rebinded j/k to scrolling, alt-j/k to send arrow keys

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,22 +1,24 @@
 config.load_autoconfig()
 
-# darkmode
 config.bind('td', 'config-cycle colors.webpage.darkmode.enabled true false')
 c.colors.webpage.preferred_color_scheme = "dark"
 c.colors.webpage.bg = '#282828'
 
-# scroll history
 config.bind('<Ctrl-j>', 'completion-item-focus next', mode='command')
 config.bind('<Ctrl-k>', 'completion-item-focus prev', mode='command')
 
-# move tabs
 config.bind('<Ctrl+j>', 'tab-move +', mode='normal')
 config.bind('<Ctrl+k>', 'tab-move -', mode='normal')
+
+config.bind("j", "scroll-px 0 100")
+config.bind("k", "scroll-px 0 -100")
+
+config.bind("<Alt-j>", "fake-key <Down>")
+config.bind("<Alt-k>", "fake-key <Up>")
 
 c.fonts.default_family = 'UbuntuMono Nerd Font'
 c.fonts.default_size = "18pt"
 
-# newpage configuration
 c.url.start_pages = ["~/.config/qutebrowser/startpage.html"]
 c.url.searchengines = {
     'DEFAULT':  'https://google.com/search?hl=en&q={}',
@@ -24,5 +26,4 @@ c.url.searchengines = {
 
 c.window.title_format = "qutebrowser"
 
-# colorscheme
 config.source('gruvbox.py')
