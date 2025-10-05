2781898fb7c465d42d68ef968cfc9626c0c5b39e
Aleksandr Shirkunov <alex.shirkunoff@gmail.com>
2025-10-05 21:02:49 +0200

    [scripts] added error handling for aur errors

--- install.sh
+++ install.sh
@@ -1,27 +1,45 @@
 #!/bin/sh
 
-command -v yay >/dev/null 2>&1 || {
-        echo "'yay' executable not found."
-        exit 1
-}
+while true; do
+        printf "Do you wish to install the dependecies? (y/n) "
+        IFS= read -r yn
 
-yay -Sy sway swaylock swaybg swayidle i3status bemenu mako wl-clipboard xdg-desktop-portal-wlr stow \
-        zsh zsh-autosuggestions zsh-syntax-highlighting neovim-nightly-bin neovim-remote neovide ranger foot tmux zathura zathura-pdf-poppler \
-        btop fastfetch pipes.sh cmatrix-git mpd ncmpcpp \
-        ttf-ubuntu-nerd ttf-ubuntu-mono-nerd
+        case $yn in
+                [Yy]* ) 
+                        command -v yay >/dev/null 2>&1 || {
+                                echo "'yay' executable not found."
+                                break
+                        }
+
+                        yay -Sy sway swaylock swaybg swayidle i3status bemenu mako wl-clipboard xdg-desktop-portal-wlr stow \
+                                zsh zsh-autosuggestions zsh-syntax-highlighting neovim-nightly-bin neovim-remote neovide \
+                                ranger foot tmux zathura zathura-pdf-poppler \
+                                btop fastfetch pipes.sh cmatrix-git mpd ncmpcpp \
+                                ttf-ubuntu-nerd ttf-ubuntu-mono-nerd || {
+
+                                echo "Some packages could not be installed!"
+                        }
+
+                        break;;
+                [Nn]* ) break;;
+                * ) echo "Please answer yes or no.";;
+        esac
+done
 
 while true; do
-        read -p "Do you wish to stow the dotfiles? (y/n) " yn
+        printf "Do you wish to stow the dotfiles? (y/n) "
+        IFS= read -r yn
+
         case $yn in
                 [Yy]* ) 
                         command -v stow >/dev/null 2>&1 || {
                                 echo "'stow' executable not found."
-                                exit 1
+                                break
                         }
 
                         cd "$HOME/.dotfiles" || {
                                 echo "Can't find the ~/.dotfiles directory."
-                                exit 1
+                                break
                         }
                         
                         for pkg in */; do
@@ -29,7 +47,7 @@ while true; do
                         done
 
                         break;;
-                [Nn]* ) exit;;
+                [Nn]* ) break;;
                 * ) echo "Please answer yes or no.";;
         esac
 done
--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 while true; do
-        read -p "Do you wish to unstow the dotfiles? (y/n) " yn
+        printf "Do you wish to unstow the dotfiles? (y/n) "
+        IFS= read -r yn
+
         case $yn in
                 [Yy]* ) 
                         command -v stow >/dev/null 2>&1 || {
-                                echo "'stow' executable not found."
-                                exit 1
+                                echo "'stow' executable not found. "
+                                break
                         }
 
                         cd "$HOME/.dotfiles" || {
                                 echo "Can't find the ~/.dotfiles directory."
-                                exit 1
+                                break
                         }
                         
                         for pkg in */; do
@@ -25,11 +27,16 @@ while true; do
 done
 
 command -v yay >/dev/null 2>&1 || {
-        echo "'yay' executable not found."
+        echo "'yay' executable not found. Exiting..."
         exit 1
 }
 
 yay -Rns sway swaylock swaybg swayidle i3status bemenu mako wl-clipboard xdg-desktop-portal-wlr \
-        zsh zsh-autosuggestions zsh-syntax-highlighting neovim-nightly-bin neovim-remote neovide ranger foot tmux zathura zathura-pdf-poppler \
+        zsh zsh-autosuggestions zsh-syntax-highlighting neovim-nightly-bin neovim-remote neovide \
+        ranger foot tmux zathura zathura-pdf-poppler \
         btop fastfetch pipes.sh cmatrix-git mpd ncmpcpp \
-        ttf-ubuntu-nerd ttf-ubuntu-mono-nerd
+        ttf-ubuntu-nerd ttf-ubuntu-mono-nerd || {
+
+        echo "Some packages could not be removed. Exiting..."
+        exit 1
+}
