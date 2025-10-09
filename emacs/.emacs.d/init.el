9e1d52ecf43e391a85c305bab6d52b05fa29a1b9
Aleksandr Shirkunov <alex.shirkunoff@gmail.com>
2025-10-09 20:20:23 +0200

    [emacs] changed the swapfile location; enabled C code formatting in the linux kernel style

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,34 +1,46 @@
-; prevents emacs from shitting inside your config
-; you'll need to load a colorscheme manually
+;; prevents emacs from shitting inside your config
+;; you'll need to load a colorscheme manually
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 (when (file-exists-p custom-file)
-        (load custom-file))
+  (load custom-file))
 
-; window decorations
+(make-directory "~/.emacs.d/auto-save/" t)
+(setq auto-save-file-name-transforms
+      `((".*" ,(concat user-emacs-directory "auto-save/") t)))
+
+;; window decorations
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
-; numberline
+;; numberline
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode t)
+(add-hook 'pdf-view-mode-hook (lambda () (display-line-numbers-mode -1)))
 
-; greeter
+;; greeter
 (setq inhibit-startup-screen t)
 
-; font
+;; tabwidth
+(setq-default tab-width 8)
+(setq-default indent-tabs-mode nil)
+
+;; font
 (set-face-attribute 'default nil
-        :font "UbuntuMono Nerd Font"
-        :height 180)
+                    :font "UbuntuMono Nerd Font"
+                    :height 180)
 
-; wrap indicator
+;; Wrap indicator
 (setq-default fringe-indicator-alist
-        (assq-delete-all 'continuation
-                (assq-delete-all 'truncation fringe-indicator-alist)))
+              (assq-delete-all 'continuation
+                               (assq-delete-all 'truncation fringe-indicator-alist)))
 
-; indentation
+;; indentation
 (setq-default tab-width 8)
 
-; pdf viewer
+;; pdf viewer
 (pdf-tools-install)
+
+;; c code style
+(setq c-default-style "linux")
