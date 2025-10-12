060c9754e96c6cacde6180d33703e7aaf2f863de
Aleksandr Shirkunov <alex.shirkunoff@gmail.com>
2025-10-12 22:31:21 +0200

    [qutebrowser] added lots of emacs-like binds; configured external editor for text fields

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -4,18 +4,6 @@ config.bind('td', 'config-cycle colors.webpage.darkmode.enabled true false')
 c.colors.webpage.preferred_color_scheme = "dark"
 c.colors.webpage.bg = '#282828'
 
-config.bind('<Ctrl-j>', 'completion-item-focus next', mode='command')
-config.bind('<Ctrl-k>', 'completion-item-focus prev', mode='command')
-
-config.bind('<Ctrl+j>', 'tab-move +', mode='normal')
-config.bind('<Ctrl+k>', 'tab-move -', mode='normal')
-
-config.bind("j", "scroll-px 0 100")
-config.bind("k", "scroll-px 0 -100")
-
-config.bind("<Alt-j>", "fake-key <Down>")
-config.bind("<Alt-k>", "fake-key <Up>")
-
 c.fonts.default_family = 'UbuntuMono Nerd Font'
 c.fonts.default_size = "18pt"
 
@@ -26,4 +14,5 @@ c.url.searchengines = {
 
 c.window.title_format = "qutebrowser"
 
-config.source('gruvbox.py')
+config.source('qutemacs.py') # keybinds
+config.source('gruvbox.py') # colorscheme
