37d7d6a0ef44cc40dfac2103ec0fa48049a2e378
Aleksandr Shirkunov <alex.shirkunoff@gmail.com>
2025-10-20 14:50:25 +0200

    [qutebrowser] added binds to manage downloads and repeate commands; fixed external editor for the third time

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,18 +1,17 @@
 config.load_autoconfig()
 
-config.bind('td', 'config-cycle colors.webpage.darkmode.enabled true false')
 c.colors.webpage.preferred_color_scheme = "dark"
-c.colors.webpage.bg = '#282828'
+c.colors.webpage.bg = "#282828"
 
-c.fonts.default_family = 'UbuntuMono Nerd Font'
+c.fonts.default_family = "UbuntuMono Nerd Font"
 c.fonts.default_size = "18pt"
 
 c.url.start_pages = ["~/.config/qutebrowser/startpage.html"]
 c.url.searchengines = {
-    'DEFAULT':  'https://google.com/search?hl=en&q={}',
+    "DEFAULT":  "https://google.com/search?hl=en&q={}",
 }
 
 c.window.title_format = "qutebrowser"
 
-config.source('qutemacs.py') # keybinds
-config.source('gruvbox.py') # colorscheme
+config.source("qutemacs.py") # keybinds
+config.source("gruvbox.py") # colorscheme
